167b79e26d7b464eadcf65472e484554c55a8849
William Kelly Teague <williamt.mine@gmail.com>
2025-02-13 09:55:42 -0600

    unique timing offsets and corrected singlerun

--- Geant4_Code/HPC_Code/HERT_FS/HERT.cc
+++ Geant4_Code/HPC_Code/HERT_FS/HERT.cc
@@ -39,7 +39,7 @@ int main(int argc,char** argv)
     //choose the Random engine
     G4Random::setTheEngine(new CLHEP::RanecuEngine());
     //set random seed with system time
-    G4long seed = StartTime(); 
+    G4long seed = StartTime()+1; 
     G4Random::setTheSeed(seed); 
     // Detect interactive mode (if no arguments) and define UI session
     G4UIExecutive* ui = 0;
